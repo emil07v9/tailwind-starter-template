3dda802c71651b20607b6a818818297bbc59d2ed
Emilie Schultz <emil07v9@stud.kea.dk>
2022-02-03 17:38:25 +0100

    .

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,12 @@
 module.exports = {
   content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      red: "#E03534",
+      beige: "#EDE6D6",
+      white: "#FFFFFF",
+      black: "#0000",
+    },
     extend: {},
   },
   plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms")],
